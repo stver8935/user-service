d6d55ff955765e4bd14893a75def3f0a211d63d8
stver8935 <gksrlgud10@naver.com>
2024-09-23 23:10:34 +0900

    feat : 메시지 관련 Config Class 추가

---
+++ src/main/java/com/stver8935/rest_api/configs/MessageConfig.java
@@ -0,0 +1,55 @@
+package com.stver8935.rest_api.configs;
+
+import com.stver8935.rest_api.utils.YamlMessageSource;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+/**
+ * @author : hangihyeong
+ * @packageName : com.stver8935.rest_api.configs
+ * @fileName : MessageConfig
+ * @since : 24. 8. 15.
+ */
+@Configuration
+public class MessageConfig {
+    @Bean
+    MessageSource generalMessageSource() {
+
+        final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages/general/GeneralMessages");
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
+    }
+
+    @Bean
+    MessageSource exceptionMessageSource() {
+        final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages/exception/ExceptionMessages");
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
+    }
+
+    @Bean
+    public MessageSource validationMessageSource() {
+
+        final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages/validation/ValidationMessages");
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+
+        final LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(validationMessageSource());
+
+        return bean;
+    }
+}
