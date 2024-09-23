703045b6cc8ce86b6a62ab8899b33508e3512bd8
stver8935 <gksrlgud10@naver.com>
2024-09-23 23:11:41 +0900

    feat : 비밀번호 암호화 config 추가

---
+++ src/main/java/com/stver8935/rest_api/configs/PasswordEncoderConfig.java
@@ -0,0 +1,20 @@
+package com.stver8935.rest_api.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author : hangihyeong
+ * @packageName : com.stver8935.rest_api.configs
+ * @fileName : PasswordEncoderConfig
+ * @since : 24. 8. 14.
+ */
+@Configuration
+public class PasswordEncoderConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
